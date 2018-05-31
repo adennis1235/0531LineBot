490870254c2f52c06e5b77e294273e22de3ff3fa
adennis1235 <adennis1235@gmail.com>
2018-05-31 14:29:04 +0800

    0

---
+++ 0531_line/Line1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Line1.aspx.cs" Inherits="_0531_line.Line1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="height: 21px" Text="Button" />
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ 0531_line/Line1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _0531_line
+{
+    public partial class Line1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            isRock.LineBot.Bot bot = new isRock.LineBot.Bot("yFDByTuynZGQnMEe7cUXmGWwkNIJpWzFEREFUtbLEj8NU2jXjgdYPyo29fsC/40A4eOXWxf2mmAfCLN7bkCRxvnzi5YeQMysj5cit9LG0z0eIjIgIiow77XFJwHsPMbKMvw3sXKoANx5XlDoLNUDowdB04t89/1O/w1cDnyilFU=");
+
+            bot.PushMessage("U3c2655e0d3e1e331fe35df961eaa070b","textxxxxxxxxxxx");
+            bot.PushMessage("U3c2655e0d3e1e331fe35df961eaa070b", 1,2);
+
+            bot.PushMessage("U3c2655e0d3e1e331fe35df961eaa070b",new Uri("https://img.meyabook.com/hotchick/upload/image/20150626/143528894336704.jpg"));
+            bot.PushMessage("U3c2655e0d3e1e331fe35df961eaa070b", new Uri("https://s2.yimg.com/bt/api/res/1.2/5Vumz_UOYRPBAoDjzl.JXw--/YXBwaWQ9eW5ld3M7cT04NTt3PTQwMA--/http://media.zenfs.com/zh-Hant_TW/News/yahoobeauty/20150622223206_easonhou_512635.jpg"));
+        }
+    }
+}
